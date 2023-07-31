9065005a9bc40b14e6b9eba45ffe3e3362b11907
federicoincitti <federico.incitti1@gmail.com>
2023-07-31 11:37:12 +0200

    modifiche navbar footer e aggiunto margin ai component sotto la nav

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -9,7 +9,6 @@ import { Component } from '@angular/core';
   <app-section-three></app-section-three>
   <app-section-four></app-section-four>
   <app-section-five></app-section-five>
-  <app-footer></app-footer>
   `,
 })
 export class HomeComponent {
